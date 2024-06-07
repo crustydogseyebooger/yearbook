e4eb4d1abafeef3690fa2b1c1e9e97d4dd1022fc
Gianna <gleidich2@gmail.com>
2024-06-07 19:32:17 -0400

    Added to notable builds

--- image_processing.js
+++ image_processing.js
@@ -258,12 +258,12 @@ function getAverage(img){
 }
 
 
-const im = Image.loadImageFromFile("images/heads/star.png");
+const im = Image.loadImageFromFile("images/notable_builds/joe_city.png");
 console.log(getAverage(im));
 
 const avg = toAverage(im);
-avg.save("star");
+avg.save("joe_city");
 
-console.log(im.width/1,im.height/1);
+console.log(im.width/2.5,im.height/2.5);
 
 
