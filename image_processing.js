358118991f8a273294a05c696ec7e07328cc2cd5
Gianna <gleidich2@gmail.com>
2024-06-08 15:11:17 -0400

    Updated members

--- image_processing.js
+++ image_processing.js
@@ -258,11 +258,11 @@ function getAverage(img){
 }
 
 
-const im = Image.loadImageFromFile("images/heads/witch.png");
+const im = Image.loadImageFromFile("images/heads/noot.png");
 console.log(getAverage(im));
 
 const avg = toAverage(im);
-avg.save("witch");
+avg.save("noot");
 
 console.log(im.width/1,im.height/1);
 
